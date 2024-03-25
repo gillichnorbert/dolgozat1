0d286240640885127daeee05064afeaee0c008f7
Bencsik Boglár Bianka <bencsikboglarbianka@ktch.hu>
2024-03-25 13:17:29 +0100

    Add project files.

---
+++ Kocka/Kocka.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KockaSol
+{
+    public class Kocka
+    {
+        public double EdgeLength { get; set; }
+
+        public Kocka(double edgeLength)
+        {
+            EdgeLength = edgeLength;
+        }
+        public double terfogatSzamitas()
+        {
+            return Math.Pow(EdgeLength, 3);
+        }
+
+        public double felszinSzamitas()
+        {
+            return 6 * Math.Pow(EdgeLength, 2);
+        }
+
+    }
+}
---
+++ Kocka/Program.cs
@@ -0,0 +1,9 @@
+﻿using KockaSol;
+
+Kocka cube = new Kocka(5);
+
+double terfogat = cube.terfogatSzamitas();
+Console.WriteLine($"A kocka térfogata: {terfogat}");
+
+double felszin = cube.felszinSzamitas();
+Console.WriteLine($"A kocka felszíne: {felszin}");
